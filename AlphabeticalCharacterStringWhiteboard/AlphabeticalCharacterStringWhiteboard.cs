e0b56ee65286b71e511f2fec71da8a4c90e12d91
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-02-13 11:12:12 -0800

    Completed the method for returning a string of alphabetical characters, given an input string

--- AlphabeticalCharacterStringWhiteboard/AlphabeticalCharacterStringWhiteboard.cs
+++ AlphabeticalCharacterStringWhiteboard/AlphabeticalCharacterStringWhiteboard.cs
@@ -6,18 +6,29 @@ namespace AlphabeticalCharacterStringWhiteboard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Given a string, return the characters of the string in alphabetical order as a string");
+            Console.WriteLine("Given a string, return the characters of the string in alphabetical order as a string\n");
+            string inputString = "Today is the 13th of February";
+            Console.WriteLine($"The alphabetical list of characters in the input string is {AlphabeticalString(inputString)}");
         }
 
 
-        public static string AlphabeticalString()
+        public static string AlphabeticalString(string inputString)
         {
+            string resultString = "";
+            inputString = inputString.ToLower();
 
-
-
-
-
-            return;
+            for (int i = 'a'; i < 'z'; i++)
+            {
+                for (int j = 0; j < inputString.Length; j++)
+                {
+                    if (inputString[j] == i)
+                    {
+                        resultString += (char)i;
+                        break;
+                    }
+                }
+            }
+            return resultString;
         }
 
 
