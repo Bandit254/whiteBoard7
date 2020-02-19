739dc557c52a199c664b1ef5d204651ed98901bb
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-02-18 19:13:07 -0800

    Added the Cube A Number and COunt Vowels project after Technical Mentoring Session on 18FEB2020

---
+++ CountVowels/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count the Vowels in a Word");
+            CountVowels();
+        }
+
+        public static void CountVowels()
+        {
+            Console.Write("Input a string to count the vowels: ");
+            string input = Console.ReadLine();
+            string lowerInput = input.ToLower();
+            int counter = 0;
+            for (int i = 0; i<input.Length; i++)
+            {
+                if (lowerInput[i] == 'a' || lowerInput[i] == 'e' || lowerInput[i] == 'i' || lowerInput[i] == 'o' || lowerInput[i] == 'u')
+                    {
+                        counter++;
+                }
+            }
+            Console.WriteLine("There are " + counter + " vowels in the string.");
+        }
+    }
+}
