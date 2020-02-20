bf73f597a74e1903ff0693667636670a9743e6de
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-02-19 18:19:43 -0800

    Added two versions of the bubble sort technique, an insertion sort, a selection sort, and answers to the quiz from 19FEB2020

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    //Taken from https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-3.php
+    public class Bubble_Sort
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = { 3, 0, 2, 5, -1, 4, 1 };
+            int t;
+            Console.WriteLine("Original array :");
+            foreach (int aa in a)
+                Console.Write(aa + " ");
+            for (int p = 0; p <= a.Length - 2; p++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                    }
+                }
+            }
+            Console.WriteLine("\n" + "Sorted array :");
+            foreach (int aa in a)
+                Console.Write(aa + " ");
+            Console.Write("\n");
+
+        }
+    }
+}
