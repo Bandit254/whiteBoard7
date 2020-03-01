2059e76622b15bd5b85682e8eab65b1102fa2677
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-02-29 21:26:52 -0800

    Added a few array projects

---
+++ ArrayValuesInOrderOfIndex/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayValuesInOrderOfIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Define an integer array of 64 values, assign each value to its corresponding index position, and then print the array to the console\n");
+            int[] myArray = CreateArray();
+            PrintArray(myArray);
+        }
+        public static int[] CreateArray()
+        {
+            int[] myArray = new int[64];
+            for (int i = 0; i < 64; i++)
+            {
+                myArray[i] = i;
+            }
+            return myArray;
+        }
+        public static void PrintArray(int[] inputArray)
+        {
+            foreach (int arrayValue in inputArray)
+            {
+                Console.Write($"{arrayValue} ");
+            }
+        }
+
+    }
+}
---
+++ Parameter_Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parameter_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Working with Parameter Arrays");
+        }
+    }
+}
