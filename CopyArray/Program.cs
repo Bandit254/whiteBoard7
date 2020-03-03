15262ea5b23980eef85abb2d12ae8f0c9e0c140d
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-03 15:43:01 -0800

    Added a project to receive an array of numbers from a user, copy the user's rray, and then print the copied array.

---
+++ CopyArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CopyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prompt the user to input an array of numbers, copy the given array, and print the copied array to the console");
+            CopyArray();
+        }
+
+        public static void CopyArray()
+        {
+            Console.WriteLine("How many numbers do you want in your array?");
+            int numItems = Convert.ToInt32(Console.ReadLine());
+
+            int[] inputArray = new int[numItems];
+            int userInput;
+
+            for (int i = 0; i < numItems; i++)
+            {
+                Console.WriteLine("Enter the next number:");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                inputArray[i] = userInput;
+            }
+
+            int[] copyArray = new int[numItems];
+
+            for (int i = 0; i < numItems; i++)
+            {
+                copyArray[i] = inputArray[i];
+            }
+
+            foreach (int x in copyArray)
+            {
+                Console.Write($"{x} ");
+            }           
+                
+            
+
+        }
+
+    }
+}
