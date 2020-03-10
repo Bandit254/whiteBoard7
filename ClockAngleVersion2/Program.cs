9ee0b073a57c505c1f67a0f0578978bed000d221
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-10 15:44:07 -0700

    Added a second version of the clock angle project, and a new project that determines if a given year is a Leap Year

---
+++ ClockAngleVersion2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClockAngleVersion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given the time in two separate numbers (hours and minutes), calculate the angle between the two clock hands.");
+            AngleOfClockHands(10, 8);
+        }
+
+        public static void AngleOfClockHands(int hour, int minutes)
+        {
+            decimal hourAngle = ((decimal)hour) * 360 / 12;
+            decimal minuteAngle = ((decimal)minutes * 360 / 60);
+            decimal clockAngle=0;
+            if (hourAngle>minuteAngle)
+            {
+                clockAngle = hourAngle - minuteAngle;
+            }
+            else if (minuteAngle>hourAngle)
+            {
+                clockAngle = minuteAngle - hourAngle;
+            }
+            Console.WriteLine($"The angle between {hour} on the hour hand and {minutes} on the minute hand is {clockAngle}");
+        }
+    }
+}
---
+++ IsLeapYear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IsLeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prompt user for a year, and determine if that year is a leap year.");
+            IsLeapYear();
+        }
+
+        public static void IsLeapYear()
+        {
+            Console.WriteLine("Please enter a 4-digit year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int diffYear = 2020 - year;
+            bool isLeapyear = false;
+            if (diffYear%4==0)
+            {
+                isLeapyear = true; 
+            }
+            Console.WriteLine($"{year} is a Leap Year, true or false? {isLeapyear}");
+        }
+
+    }
+}
