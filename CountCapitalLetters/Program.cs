c06ac3c7e1cced5d0d7c9922337a6d313568134c
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-10 11:33:23 -0700

    Added a project to count the capital letters in a string, and a project that reads a text file and performs various operations on it

---
+++ CountCapitalLetters/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CountCapitalLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given a string with both upper and lower case letters, write a method that returns the number of capital letters in the string:\n");
+            string inputString = "Hello World!";
+            
+            Console.WriteLine($"The number of capital letters in '{inputString}' is {CountCapitalLetters(inputString)}");
+
+        }
+
+
+        public static int CountCapitalLetters(string inputString)
+        {
+            int count = 0;
+            string capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                for (int j = 0; j < capitalLetters.Length; j++)
+                {
+                    if (String.Equals(inputString[i], capitalLetters[j]))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+
+    }
+}
---
+++ FileIO/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //ReadTextFile();
+            //PrintFileToConsole();
+            //CountTextFileLines();
+            //CountEmpyLines();
+            //CountWords();
+            CountUniqueWords();
+        }
+        public static void ReadTextFile()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+        }
+
+        public static void PrintFileToConsole()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+        }
+        public static void CountTextFileLines()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+            Console.WriteLine($"The number of lines in the song is {lines.Length}");
+        }
+        public static void CountEmpyLines()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+            Console.WriteLine($"The number of lines in the song is {lines.Length}");
+            int counter = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] =="")
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"The number of lines in the song is {lines.Length}, {counter} of which are empty (blank spaces between verses)");
+        }
+        public static void CountWords()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+            string[] words = text.Split(' ');
+            int numWords = words.Length;
+            Console.WriteLine($"The number of words in the song is {numWords}");
+        }
+
+        public static void CountUniqueWords()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\pc\Desktop\Eisbrecher_Metall_Lyrics");
+            string[] words = text.Split(' ');
+            int numUniqueWords = 0;
+            int numRepeatedWords = 0;
+            
+            for (int i = 0; i < words.Length-1; i++)
+            {
+                bool isDistinct = true;
+                for (int j = i+1; j < words.Length; j++)
+                {
+                    if (words[i]==words[j])
+                    {
+                        isDistinct = false;
+                        
+                        //break;
+                    }
+                }
+                if (isDistinct)
+                {
+                    numUniqueWords++;
+                }
+            }
+            //numUniqueWords = words.Length - numRepeatedWords;
+            Console.WriteLine($"The number of unique words in the song is {numUniqueWords}");
+
+        }
+
+    }
+}
