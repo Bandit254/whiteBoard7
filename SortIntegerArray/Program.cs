9866ee48ccc6a5c1228e8681e54cd132df2fbb57
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-24 16:01:09 -0700

    Added a project that sorts an integer array using a bubble sort technique

---
+++ SortIntegerArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SortIntegerArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given an integer array, sort the integers from least to greatest.\n");
+            int[] inputArray = new int[] { 5, 2, 6, 1, 7 };
+            int[] sortedArray = SortIntegerArray(inputArray);
+            PrintArray(sortedArray);
+        }
+
+        public static int[] SortIntegerArray(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i]< inputArray[j])
+                    {
+                        int x = inputArray[i];
+                        inputArray[i] = inputArray[j];
+                        inputArray[j] = x;
+                    }
+
+                }
+            }
+
+            return inputArray;
+        }
+        public static void PrintArray(int[] inputArray)
+        {
+            foreach (int x in inputArray)
+            {
+                Console.Write($"{x} ");
+            }
+        }
+    }
+}
