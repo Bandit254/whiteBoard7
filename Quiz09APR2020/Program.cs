a2e7d23d39e9804cfc35c758c1f254bc8ef52e8d
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-13 11:51:32 -0700

    Added a simple shift cypher to encryt/decrypt strings, as well as the answers to a quiz from 9 April.

---
+++ Quiz09APR2020/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Quiz09APR2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quiz answers from 09APR2020.\n");
+            Console.WriteLine($"{Question1()}");
+            Console.WriteLine($"{Question2()}");
+            Console.WriteLine($"{Question3()}");
+            Console.WriteLine($"{Question4()}");
+            Console.WriteLine($"{Question5()}");
+            Console.WriteLine($"{Question6()}");
+        }
+        public static int Question1()
+        {
+            int y = 5;
+            int x = 1;
+            do
+            {
+                x += y;
+            } while (y-- > 1);
+
+            return x;
+        }
+        public static bool Question2()
+        {
+            bool x;
+            int a = 4;
+            int b = 4;
+            a = ++a;
+            b = b & 1;
+            x = (a == b);
+            return x;
+        }
+        public static int Question3(int x = 5)
+        {
+            int y = 2;
+            if (x > y)
+            {
+                x %= y;
+                Question3(x);
+            }
+            return x;
+        }
+        public static int Question4()
+        {
+            int x = 0;
+            int y = 5;
+
+            for (int i = y; i > 0; i--)
+            {
+                x += (i % 2 == 1) ? i : y;
+            }
+            return x;
+        }
+        public static int Question5()
+        {
+            int x = 0;
+            int y = 3;
+            bool isTrue = true;
+            while (!isTrue)
+            {
+                x += ++y;
+                if (x >= 12)
+                {
+                    isTrue = (!false);
+                }
+            }
+            return x;
+        }
+        public static int Question6()
+        {
+            string string1 = "Mississippi";
+            string string2 = "";
+            foreach (var s in string1.Split('i'))
+            {
+                string2 += s;
+            }
+            int x = string2.Length;
+            return x;
+        }
+    }
+}
---
+++ ShiftCypher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ShiftCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implement a shift-cypher that can encrypt a plain text message by shifting a certain number of characters. It should also be able to receive an encrypted message and decrypt it by providing the number of spaces used to shift the message originally.\n");
+            Console.WriteLine("\nAssume the input strings are in all-caps.\n");
+            string encryptedMessage = EncryptMessage("HELLO", 3);
+            Console.WriteLine($"{EncryptMessage("HELLO", 3)}.");
+            Console.WriteLine($"{EncryptMessage(encryptedMessage, -3)}.");
+        }
+        public static string EncryptMessage(string input, int numPlaces)
+        {
+            string encryptedMessage = "";
+            char currentChar=' ';
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentChar = (char)(input[i] + numPlaces);
+                if (currentChar<'A')
+                {
+                    int leftOver = Math.Abs(currentChar - 'A');
+                    currentChar = (char)('Z' + 1 - leftOver);
+                }
+                if (currentChar>'Z')
+                {
+                    int leftOver = Math.Abs(currentChar - 'Z');
+                    currentChar = (char)('A' - 1 + leftOver);
+                }
+                encryptedMessage += currentChar;
+            }
+            return encryptedMessage;
+        }
+    }
+}
