ecc8ef556cd1a17d65f06e1f691099bf7d8617a0
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-05-12 11:17:57 -0700

    Added a project to sort an integer array and another project to calculate the score of a word in Scrabble

---
+++ ArrayIntegerSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayIntegerSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sort an array of integers.");
+
+            int[] input = new int[] { 4, 2, 7, 1, 10 };
+            int[] sortedArray = ArrayIntegerSort(input);
+            foreach (int x in sortedArray)
+            {
+                Console.Write($"{x} ");
+            }
+        }
+
+        public static int[] ArrayIntegerSort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j]>input[i])
+                    {
+                        int temp = input[j];
+                        input[j] = input[i];
+                        input[i] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+    }
+}
---
+++ Scrabble/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scrabble Game\n");
+            //string input = "apple";
+            //Console.WriteLine($"The point value of {input} is {ScrabbleScore(input)}");
+
+            List<string> inputWords = new List<string>();
+            int x = 1;
+            while (x==1)
+            {
+                inputWords.Add(Console.ReadLine());
+                Console.Write("Enter 1 to keep going: ");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+            int totalScore = 0;
+            foreach (string s in inputWords)
+            {
+                totalScore += ScrabbleScore(s);
+            }
+            Console.WriteLine($"Total Score is {totalScore}");
+            Console.ReadKey();
+        }
+        public static int[] PointValues = new int[] { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 4, 4, 8, 4, 10 };
+        public static int ScrabbleScore(string input)
+        {
+            string inputUpper = input.ToUpper();
+            int score = 0;
+            for (int i = 0; i < inputUpper.Length; i++)
+            {
+                score += PointValues[(int)(inputUpper[i]-'A')];
+            }
+            return score;
+        }
+    }
+}
