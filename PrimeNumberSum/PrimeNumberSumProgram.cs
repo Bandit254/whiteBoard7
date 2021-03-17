476697b30cb3e31e682ada613cd7a1f669e67ef2
Zachary Silvis <zachary.w.silvis@outlook.com>
2021-03-17 10:30:55 -0400

    Updated the PrimeNumberSumProgram

--- PrimeNumberSum/PrimeNumberSumProgram.cs
+++ PrimeNumberSum/PrimeNumberSumProgram.cs
@@ -7,67 +7,36 @@ namespace PrimeNumberSum
         static void Main(string[] args)
         {
             Console.WriteLine("Return the sum of an arbitrary number of prime numbers");
+            int numPrimes = 5;
+            Console.WriteLine($"The sum of the first {numPrimes} prime numbers is {PrimeNumberSum(numPrimes)}");
         }
-
         public static int PrimeNumberSum(int numberOfPrimes)
         {
             int sumOfPrimes = 0;
             int counter = 0;
-            //int nextNumber = 2;
-
+            int nextNumber = 2;
+            bool isPrime = false;
             while(counter<numberOfPrimes)
             {
-
+                isPrime = CheckPrime(nextNumber);
+                if (isPrime)
+                {
+                    sumOfPrimes += nextNumber;
+                    counter++;
+                }
+                nextNumber++;
             }
-
-
-
-
-
             return sumOfPrimes;
         }
-
-        public static bool IsPrime(int num)
+        public static bool CheckPrime(int num)
         {
             if (num < 2) return false;
+            if (num == 2) return true;
             for (int i = 2; i < num; i++)
             {
                 if (num % i == 0) return false;
             }
             return true;
         }
-
-        /*
-
-                public static int PrimeSum( int n )
-        {
-            int sumOfPrimes = 0;
-            int numberOfPrimes = 0;
-
-            int nextInteger = 2;
-            while(numberOfPrimes < n)
-            {
-                if(IsPrime(nextInteger))
-                {
-                    sumOfPrimes += nextInteger;
-                    numberOfPrimes++;
-                }
-                nextInteger++;
-            }
-
-            return sumOfPrimes;
-        }
-        public static bool IsPrime( int num )
-        {
-            if (num < 2) return false;
-            for (int i = 2; i < num; i++)
-            {
-                if( num % i == 0 ) return false;
-            }
-            return true;
-        }
-
-
-        */
     }
 }
